e87b218d8fc3b58853ddc5a67929f409c582fae6
Phùng Mạnh Đức <phungmanhduc2209@gmail.com>
2024-05-22 11:25:02 +0700

    PMD_Update_Giao_Dien

---
+++ frontend/src/views/backend/users/IndexUser.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Layout from '@/components/backend/Layout.vue'
+</script>
+
+<template>
+    <Layout>
+        <template #AdminContent>
+            <div class="row project-wrapper">
+                Đây là Quản Lý User
+            </div>
+        </template>
+    </Layout>
+</template>
+
+<style scoped></style>
